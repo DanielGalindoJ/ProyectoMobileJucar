7f462ae8c4fb725fc5eece36df1a95431abe12dc
Daniel Galindo <galijine@gmail.com>
2023-11-20 19:02:48 -0500

    Add navigation components

---
+++ src/components/navigation/AppNavigation.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+export function AppNavigation(){
+    return(
+        <View>
+            <Text>AppNavigation</Text>
+            <Text>AppNavigation</Text>
+            <Text>AppNavigation</Text>
+            <Text>AppNavigation</Text>
+            <Text>AppNavigation</Text>
+            <Text>AppNavigation</Text>
+        </View>
+    )
+}
---
+++ src/components/navigation/RootNavigation.js
@@ -0,0 +1,8 @@
+import { AuthScreen } from "../screens/Auth";  
+import { AppNavigation } from "./AppNavigation";
+
+export function RootNavigation(){
+        const user =null;
+
+    return user  ? <AppNavigation/> : <AuthScreen/>
+}
