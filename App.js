acfca2024f9993b8bcbfbe8192e5d0ba766c818e
Daniel Galindo <galijine@gmail.com>
2023-11-21 20:52:17 -0500

    Commented out Menu import

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import { StyleSheet, Text, View, Image,TextInput, Button,Pressable,Alert,StatusBar } from 'react-native';
 import { useState } from "react";
-import Menu from '../../src/components/Menu';
+//import Menu from '../../src/components/Menu';
 // import logo from './assets/jucar'
 //import { StatusBar } from 'expo-status-bar';
 import { PaperProvider } from 'react-native-paper';
