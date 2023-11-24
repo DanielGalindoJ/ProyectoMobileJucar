50bdaf8dc0db3dbf99afc39014b1bfc35c45e647
Daniel Galindo <galijine@gmail.com>
2023-11-24 11:22:32 -0500

    Add formAgregarProduct component

---
+++ src/components/formAgregarProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, Pressable, ScrollView, Modal } from 'react-native';
+import { useState } from 'react';
+
+const FormAgregarProduct = () => {
+    return (
+     
+     <View>
+            {/* Your code here */}
+        </View>
+    );
+};
